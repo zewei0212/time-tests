0529f198727928148f314072453dcb7457aa914e
zewei0212 <zcapzwh@ucl.ac.uk>
2025-10-27 00:47:33 +0000

    Test again anf add the comment.

--- test_times.py
+++ test_times.py
@@ -1,5 +1,9 @@
 import times  
 def test_given_input():
+    """The content in expected cell is directly copied from the result 
+    of times.py. The purpose of this function is test whether the results
+    match, if match the result will be pass when input pytest -v
+    in the terminal."""
     
     large = times.time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = times.time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
